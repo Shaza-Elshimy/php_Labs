64ed5c5296cf4d5560f871e83c6cfbdd2be1d0a1
shaza <shazahamdy011@gmail.com>
2026-03-02 20:01:14 +0200

    add day1

---
+++ day1/done.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Review</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif
+        }  
+        
+        h2{
+            color: #333;
+        }
+        h3{
+            color: #555;
+}
+            </style>
+
+</head>
+<body>
+    <?php
+        $text =($_POST['gender']=="Male")?"Mr.":"Ms.";
+
+        echo "<h2>Thanks $text $_POST[fname] $_POST[lname]</h2>";
+        echo "<h2>please review your information</h2>";
+        echo "<h3>Name: " . $_POST['fname'] . " " . $_POST['lname'] . "</h3>";
+        echo "<h3>Address: " . $_POST['address'] . "</h3>";
+
+        echo "Your skills: <br>";
+
+        echo "<ul>";
+        if (!empty($_POST['skills'])) {
+            foreach($_POST['skills'] as $skill){
+                echo "<li>$skill</li>";
+            }
+        }
+       echo "</ul>";
+        echo "<h3>Department: " . $_POST['department'] . "</h3>";
+        ?>
+</body>
+</html>
